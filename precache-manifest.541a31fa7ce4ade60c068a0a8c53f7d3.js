d94f6c887fc3ac80b670179ce17518661a4c725a
Josce <nanayhachi94@gmail.com>
2018-11-14 16:40:19 -0600

    Updates

---
+++ precache-manifest.541a31fa7ce4ade60c068a0a8c53f7d3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "7de40858973be71c5825",
+    "url": "/my-portafolio/static/css/main.c2ceed90.chunk.css"
+  },
+  {
+    "revision": "7de40858973be71c5825",
+    "url": "/my-portafolio/static/js/main.7de40858.chunk.js"
+  },
+  {
+    "revision": "d1de318241312b249aad",
+    "url": "/my-portafolio/static/js/1.d1de3182.chunk.js"
+  },
+  {
+    "revision": "9ac6009e2f3cb8594608",
+    "url": "/my-portafolio/static/js/runtime~main.9ac6009e.js"
+  },
+  {
+    "revision": "6c0f0254f30f65740ba809b52c36a532",
+    "url": "/my-portafolio/static/media/me.6c0f0254.jpg"
+  },
+  {
+    "revision": "91030d938e97ddf4d45594076628b97e",
+    "url": "/my-portafolio/static/media/colores.91030d93.jpg"
+  },
+  {
+    "revision": "8ca8684bb88e469f7236d2062e05b460",
+    "url": "/my-portafolio/static/media/londres.8ca8684b.jpeg"
+  },
+  {
+    "revision": "492e2b8c65fee4deda22f7de1acb4bed",
+    "url": "/my-portafolio/static/media/paris.492e2b8c.jpg"
+  },
+  {
+    "revision": "d79728b930663c8531ab6ab88ffc25e5",
+    "url": "/my-portafolio/static/media/git.d79728b9.jpg"
+  },
+  {
+    "revision": "e7c73a8818ddebef3b6cf512f1070991",
+    "url": "/my-portafolio/static/media/gmail.e7c73a88.jpg"
+  },
+  {
+    "revision": "78954f2eefce27c2109604ad52045def",
+    "url": "/my-portafolio/static/media/lila.78954f2e.jpg"
+  },
+  {
+    "revision": "76e2982658dcd30bf4da22d6596e1b0f",
+    "url": "/my-portafolio/index.html"
+  }
+];
