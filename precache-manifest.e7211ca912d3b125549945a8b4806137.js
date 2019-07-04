3ab7fc867872df826071dbeea13f320bee4712d7
joscejaimes <nanayhachi94@gmail.com>
2019-07-04 14:05:36 -0500

    Updates

---
+++ precache-manifest.e7211ca912d3b125549945a8b4806137.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbd4793b1d9699d18748ccf20d015972",
+    "url": "/my-portafolio/index.html"
+  },
+  {
+    "revision": "f2ffbbfdaf35b7138347",
+    "url": "/my-portafolio/static/css/main.9c34cd00.chunk.css"
+  },
+  {
+    "revision": "22e1c9b50d3f223b749f",
+    "url": "/my-portafolio/static/js/2.6f410304.chunk.js"
+  },
+  {
+    "revision": "f2ffbbfdaf35b7138347",
+    "url": "/my-portafolio/static/js/main.0d273375.chunk.js"
+  },
+  {
+    "revision": "bc5eea72434c9276a072",
+    "url": "/my-portafolio/static/js/runtime~main.6e99377c.js"
+  },
+  {
+    "revision": "83b60641123ee929ecdd8b9e854704e2",
+    "url": "/my-portafolio/static/media/Blockchain.83b60641.png"
+  },
+  {
+    "revision": "2c45f0e9478e9fbc4f9e3c4750213b8f",
+    "url": "/my-portafolio/static/media/bootstap.2c45f0e9.png"
+  },
+  {
+    "revision": "eec36a3fa2bb96d67675210f9e90424f",
+    "url": "/my-portafolio/static/media/dashboard.eec36a3f.png"
+  },
+  {
+    "revision": "e62696d75c8bbf868210668ff7f91a17",
+    "url": "/my-portafolio/static/media/foodmap.e62696d7.jpg"
+  },
+  {
+    "revision": "d79728b930663c8531ab6ab88ffc25e5",
+    "url": "/my-portafolio/static/media/git.d79728b9.jpg"
+  },
+  {
+    "revision": "e7c73a8818ddebef3b6cf512f1070991",
+    "url": "/my-portafolio/static/media/gmail.e7c73a88.jpg"
+  },
+  {
+    "revision": "c2d87fdde30423cab85f0d37c9858565",
+    "url": "/my-portafolio/static/media/javascript.c2d87fdd.png"
+  },
+  {
+    "revision": "4184ea02b22eb0749018402a1b2c9d11",
+    "url": "/my-portafolio/static/media/moviapp.4184ea02.jpg"
+  },
+  {
+    "revision": "3a515bee65604398c4cd47db57785e7c",
+    "url": "/my-portafolio/static/media/perfil.3a515bee.jpeg"
+  },
+  {
+    "revision": "40342f942838db49734449422b801887",
+    "url": "/my-portafolio/static/media/react-native.40342f94.png"
+  },
+  {
+    "revision": "37fe8322b169ddbdeabf75930e886ac6",
+    "url": "/my-portafolio/static/media/react.37fe8322.png"
+  }
+]);
